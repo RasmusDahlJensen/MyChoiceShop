688af3d15649aca1f7f754002ffdae77cf6cd809
Rasmus Jensen <rasmusdj96@gmail.com>
2023-06-20 12:18:19 +0200

    Added sequelize to supabase

--- config/db.sequelize.js
+++ config/db.sequelize.js
@@ -1,10 +1,8 @@
-import { Sequelize } from "sequelize"
-import dotenv from "dotenv"
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const sequelize = new Sequelize(
-   process.env.DATABASE_URL
-)
-
-export {sequelize}
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+	dialect: "postgres",
+});
--- index.js
+++ index.js
@@ -1,18 +1,60 @@
-import express from "express"
+import { Sequelize, DataTypes } from "sequelize";
+import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
-import mysql from "mysql2";
-import { sequelize } from "./config/db.sequelize.js";
 
-const app = express();
+dotenv.config();
 
-app.listen(4000, async () => {
-    console.clear()
-    console.log("listening on port 4000");
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-})
+// Create a Sequelize instance
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+	dialect: "postgres",
+});
 
+// Define a model using Sequelize
+const User = sequelize.define("User", {
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	email: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		unique: true,
+	},
+});
+
+// Initialize Supabase client
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Sync the Sequelize models with the database
+sequelize
+	.sync()
+	.then(() => {
+		console.log("Sequelize models synced with the database.");
+
+		// Perform database operations using Sequelize
+		// Example: Create a new user
+		User.create({ name: "John Doe", email: "johndoe@example.com" })
+			.then((user) => {
+				console.log("User created:", user.toJSON());
+			})
+			.catch((error) => {
+				console.error("Failed to create user:", error);
+			});
+	})
+	.catch((error) => {
+		console.error("Sequelize sync error:", error);
+	});
+
+// Perform Supabase operations
+// Example: Retrieve all users from Supabase
+supabase
+	.from("users")
+	.select("*")
+	.then((response) => {
+		console.log("Users retrieved from Supabase:", response.data);
+	})
+	.catch((error) => {
+		console.error("Failed to retrieve users from Supabase:", error);
+	});
