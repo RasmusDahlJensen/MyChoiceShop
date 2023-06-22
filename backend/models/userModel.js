5775f842fdd846d01c307b86d62165f32077de4f
Rasmus Jensen <rasmusdj96@gmail.com>
2023-06-22 12:29:04 +0200

    Removed userModel login method, not needed.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,29 +1,7 @@
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../config/db.sequelize.js";
 
-class UserModel extends Model {
-	// Static method to find a user by their credentials
-	static async findByCredentials(email, password) {
-		// Find a user in the database based on the provided email
-		const user = await UserModel.findOne({ where: { email } });
-
-		// If no user is found, throw an error indicating invalid email or password
-		if (!user) {
-			throw new Error("Invalid email or password");
-		}
-
-		// Compare the provided password with the hashed password stored in the database
-		const isMatch = await bcrypt.compare(password, user.password);
-
-		// If the passwords don't match, throw an error indicating invalid email or password
-		if (!isMatch) {
-			throw new Error("Invalid email or password");
-		}
-
-		// If the user is found and the password matches, return the user object
-		return user;
-	}
-}
+class UserModel extends Model {}
 
 UserModel.init(
 	{
